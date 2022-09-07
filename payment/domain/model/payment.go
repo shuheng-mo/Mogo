90971ecd4f120c797a53ec9f4dd53052dfacf45b
acse-sm321 <shuheng.mo21@imperial.ac.uk>
2022-09-07 17:52:40 +0100

    Updated common method

--- payment/domain/model/payment.go
+++ payment/domain/model/payment.go
@@ -1,6 +1,9 @@
 package model
 
-type Payment struct{
-	ID int64 `gorm:"primary_key;not_null;auto_increment"`
+type Payment struct {
+	ID           int64  `gorm:"primary_key;not_null;auto_increment"`
+	PaymentName  string `json:"payment_name"`
+	PaymentSID   string `json:"payment_sid"`
+	PaymentStats bool   `json:"payment_stats"`
+	PaymentImage string `json:"payment_image"`
 }
-
--- payment/handler/payment.go
+++ payment/handler/payment.go
@@ -1,47 +1,25 @@
 package handler
 
 import (
-	"context"
+	"github.com/acse-sm321/Mogo/common"
+	"github.com/acse-sm321/Mogo/payment/domain/model"
 	"github.com/acse-sm321/Mogo/payment/domain/service"
+	"context"
 	payment "github.com/acse-sm321/Mogo/payment/proto/payment"
-	log "github.com/micro/go-micro/v2/logger"
 )
 
 type Payment struct {
 	PaymentDataService service.IPaymentDataService
 }
 
-// Call is a single request handler called via client.Call or the generated client code
-func (e *Payment) Call(ctx context.Context, req *payment.Request, rsp *payment.Response) error {
-	log.Info("Received Payment.Call request")
-	rsp.Msg = "Hello " + req.Name
-	return nil
-}
-
-// Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *Payment) Stream(ctx context.Context, req *payment.StreamingRequest, stream payment.Payment_StreamStream) error {
-	log.Infof("Received Payment.Stream request with count: %d", req.Count)
-	for i := 0; i < int(req.Count); i++ {
-		log.Infof("Responding: %d", i)
-		if err := stream.Send(&payment.StreamingResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *Payment) PingPong(ctx context.Context, stream payment.Payment_PingPongStream) error {
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		log.Infof("Got ping %v", req.Stroke)
-		if err := stream.Send(&payment.Pong{Stroke: req.Stroke}); err != nil {
-			return err
-		}
+func (e *Payment) AddPayment(ctx context.Context, request *payment.PaymentInfo, response *payment.PaymentID) error{
+	payment := &model.Payment{}
+	if err := common.SwapTo(request,payment);err!=nil{
+		
 	}
 }
+
+UpdatePayment(ctx context.Context, in *PaymentInfo, out *Response) error
+DeletePaymentByID(ctx context.Context, in *PaymentID, out *Response) error
+FindPaymentByID(ctx context.Context, in *PaymentID, out *PaymentInfo) error
+FindAllPayment(ctx context.Context, in *All, out *PaymentAll) error
